eb86611391bda3e077f2ed3f8eef7118f22975bb
waguilar29 <33886272+waguilar29@users.noreply.github.com>
2018-01-16 23:57:53 -0800

    Initial commit

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyCubtwtkDLDXeYsCuE_-Ay--UVfJzVo1tA",
+  authDomain: "first-7782d.firebaseapp.com",
+  databaseURL: "https://first-7782d.firebaseio.com",
+  projectId: "first-7782d",
+  storageBucket: "first-7782d.appspot.com",
+  messagingSenderId: "467132183749"
+};
+
+  module.exports = config;
---
+++ src/components/shared/PeqQuestions.js
@@ -0,0 +1,63 @@
+export default {
+    Satisfaction: [
+      {
+        id: '1A',
+        question: 'How happy you have been with your current prosthesis',
+        category: 'Satisfaction'
+      },
+      {
+        id: '1B',
+        question: 'How satisfied you have been with your prosthesis',
+        category: 'Satisfaction'
+      },
+      {
+        id: '1C',
+        question: 'How satisfied you have been with how you are walking',
+        category: 'Satisfaction'
+      },
+    ],
+    Utility: [
+      {
+        id: '2A',
+        question: 'The fit of your prosthesis',
+        category: 'Utility'
+      },
+      {
+        id: '2B',
+        question: 'The weight of your prosthesis',
+        category: 'Utility'
+      },
+      {
+        id: '2C',
+        question: 'Your comfort while standing when using your prosthesis',
+        category: 'Utility'
+      },
+      {
+        id: '2D',
+        question: 'Your comfort while sitting when using your prosthesis',
+        category: 'Utility'
+      },
+      {
+        id: '2E',
+        question: 'How often you felt off balance while using your prosthesis',
+        category: 'Utility'
+      },
+      {
+        id: '2F',
+        question: 'How much energy it took to use your prosthesis for as long as you needed it.',
+        category: 'Utility'
+      },
+      {
+        id: '2G',
+        question: 'The feel (such as the temperature and texture) of the prosthesis (sock, liner, socket) on your residual limb (stump)',
+        category: 'Utility'
+      },
+      {
+        id: '2H',
+        question: 'The ease of putting on (donning) your prosthesis',
+        category: 'Utility'
+      }
+    ]
+
+
+  }
